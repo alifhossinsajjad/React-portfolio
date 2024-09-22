a76f0b31189dd983e5729e8b597b72a9ba7f6f01
MD ALIF MIA <142040065+alifhossinsajjad@users.noreply.github.com>
2024-09-22 11:48:08 +0600

    initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:alifhossinsjjad123456@email.com">
+            alifhossinsjjad123456@gmail.com
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/alif-mia-320039276/">
+            linkedin.com/Alif mia
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/alifhossinsajjad">
+            github.com/MD ALIF MIA
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
